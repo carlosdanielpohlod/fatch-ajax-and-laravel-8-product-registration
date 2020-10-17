2ebe6d13774e92d8124468e817f9325c1f9bafbf
carlosdanielpohlod <carlospohlod@gmail.com>
2020-10-17 15:20:24 -0300

    Nomeada a tabela Products para product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
 {
     use HasFactory;
     public $primaryKey = 'idProduct';
-    public $table = 'Product';
+    public $table = 'product';
     public $fillable = ['idProduct', 'name','price'];
     public $timestamps = false;
     public function new($data){
