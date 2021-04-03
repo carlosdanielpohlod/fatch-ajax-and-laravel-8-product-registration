b67d2cfd2edff10602d0a9b00809e478309ed3d3
carlosdanielpohlod <carlospohlod@gmail.com>
2021-04-03 11:45:09 -0300

    Concluido o banco, porem com erros nos ultimos alter

--- database/migrations/2021_04_03_141234_alter_users_table.php
+++ database/migrations/2021_04_03_141234_alter_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCreditCardUserTable extends Migration
+class AlterUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateCreditCardUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('credit_card_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('surName');
         });
     }
 
@@ -26,6 +25,6 @@ class CreateCreditCardUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('credit_card_user');
+        //
     }
 }
